f5dd43a9d10abb2de50956d23505da08896f0c64
Michael Clark <michael@metaparadigm.com>
2009-08-27 06:40:00 +0000

    * Fix subtle bug in linkhash where lookup could hang after all slots     were filled then successively freed.     Spotted by Jean-Marc Naud, j dash m at newtraxtech dot com
    git-svn-id: http://svn.metaparadigm.com/svn/json-c/trunk@54 327403b1-1117-474d-bef2-5cb71233fd97

--- linkhash.c
+++ linkhash.c
@@ -158,13 +158,15 @@ struct lh_entry* lh_table_lookup_entry(struct lh_table *t, const void *k)
 {
 	unsigned long h = t->hash_fn(k);
 	unsigned long n = h % t->size;
+	int count = 0;
 
 	t->lookups++;
-	while( 1 ) {
+	while( count < t->size ) {
 		if(t->table[n].k == LH_EMPTY) return NULL;
 		if(t->table[n].k != LH_FREED &&
 		   t->equal_fn(t->table[n].k, k)) return &t->table[n];
 		if(++n == t->size) n = 0;
+		count++;
 	}
 	return NULL;
 }
