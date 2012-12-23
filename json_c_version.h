fcc768e6679076ef90ea263569a36ca29a85a544
Eric Haszlakiewicz <erh+git@nimenees.com>
2012-12-23 11:09:20 -0600

    Bump the version up to 0.10.99 to make it clear that the master branch is beyond anything on the 0.10 branch.

--- json_c_version.h
+++ json_c_version.h
@@ -10,11 +10,11 @@
 
 #define JSON_C_MAJOR_VERSION 0
 #define JSON_C_MINOR_VERSION 10
-#define JSON_C_MICRO_VERSION 0
+#define JSON_C_MICRO_VERSION 99
 #define JSON_C_VERSION_NUM ((JSON_C_MAJOR_VERSION << 16) | \
                             (JSON_C_MINOR_VERSION << 8) | \
                             JSON_C_MICRO_VERSION)
-#define JSON_C_VERSION "0.10"
+#define JSON_C_VERSION "0.10.99"
 
 const char *json_c_version(void); /* Returns JSON_C_VERSION */
 int json_c_version_num(void);     /* Returns JSON_C_VERSION_NUM */
