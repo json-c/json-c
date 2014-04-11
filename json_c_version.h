515ba0dfb7d4def2f7f5f8c7ed78a729328666fe
Eric Haszlakiewicz <erh+git@nimenees.com>
2014-04-10 22:44:13 -0400

    Bump versions up to 0.12.99 since a 0.12 release was just created.

--- json_c_version.h
+++ json_c_version.h
@@ -9,12 +9,12 @@
 #define _json_c_version_h_
 
 #define JSON_C_MAJOR_VERSION 0
-#define JSON_C_MINOR_VERSION 11
+#define JSON_C_MINOR_VERSION 12
 #define JSON_C_MICRO_VERSION 99
 #define JSON_C_VERSION_NUM ((JSON_C_MAJOR_VERSION << 16) | \
                             (JSON_C_MINOR_VERSION << 8) | \
                             JSON_C_MICRO_VERSION)
-#define JSON_C_VERSION "0.11.99"
+#define JSON_C_VERSION "0.12.99"
 
 const char *json_c_version(void); /* Returns JSON_C_VERSION */
 int json_c_version_num(void);     /* Returns JSON_C_VERSION_NUM */
