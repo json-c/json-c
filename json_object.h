37f5d8696d2c60d106dbba9fe73a5ead40b8fd19
Alexander Dahl <ada@ifak-system.com>
2014-08-18 10:28:38 +0200

    improve doc for json_object_to_json_string()

--- json_object.h
+++ json_object.h
@@ -161,12 +161,17 @@ extern enum json_type json_object_get_type(struct json_object *obj);
 
 /** Stringify object to json format.
  * Equivalent to json_object_to_json_string_ext(obj, JSON_C_TO_STRING_SPACED)
+ * The pointer you get is an internal of your json object. You don't
+ * have to free it, later use of json_object_put() should be sufficient.
+ * If you can not ensure there's no concurrent access to *obj use
+ * strdup().
  * @param obj the json_object instance
  * @returns a string in JSON format
  */
 extern const char* json_object_to_json_string(struct json_object *obj);
 
 /** Stringify object to json format
+ * @see json_object_to_json_string() for details on how to free string.
  * @param obj the json_object instance
  * @param flags formatting options, see JSON_C_TO_STRING_PRETTY and other constants
  * @returns a string in JSON format
