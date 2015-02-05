720d566d03e23418b7bb3cff2f74ef7d95c6a459
James Myatt <james.myatt@tessella.com>
2015-02-05 15:58:12 +0000

    Define macros from inttypes.h when not available

--- json_inttypes.h
+++ json_inttypes.h
@@ -4,24 +4,15 @@
 
 #include "json_config.h"
 
-#if defined(_MSC_VER) && _MSC_VER <= 1700
-
-/* Anything less than Visual Studio C++ 10 is missing stdint.h and inttypes.h */
-typedef __int32 int32_t;
-#define INT32_MIN    ((int32_t)_I32_MIN)
-#define INT32_MAX    ((int32_t)_I32_MAX)
-typedef __int64 int64_t;
-#define INT64_MIN    ((int64_t)_I64_MIN)
-#define INT64_MAX    ((int64_t)_I64_MAX)
-#define PRId64 "I64d"
-#define SCNd64 "I64d"
+#ifdef JSON_C_HAVE_INTTYPES_H
+/* inttypes.h includes stdint.h */
+#include <inttypes.h>
 
 #else
+#include <stdint.h>
 
-#ifdef JSON_C_HAVE_INTTYPES_H
-#include <inttypes.h>
-#endif
-/* inttypes.h includes stdint.h */
+#define PRId64 "I64d"
+#define SCNd64 "I64d"
 
 #endif
 
