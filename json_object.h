89ae583a8624fad6be4a7d1da084b0c410e4fc63
Stuart Walsh <stu@ipng.org.uk>
2015-03-31 12:23:03 +0100

    Fix uninitialised variable compile warning, and also fix unused-when-used warning

--- json_object.h
+++ json_object.h
@@ -339,8 +339,8 @@ extern void json_object_object_del(struct json_object* obj, const char *key);
 #if defined(__GNUC__) && !defined(__STRICT_ANSI__) && __STDC_VERSION__ >= 199901L
 
 # define json_object_object_foreach(obj,key,val) \
-	char *key; \
-	struct json_object *val __attribute__((__unused__)); \
+	char *key = NULL; \
+	struct json_object *val = NULL; \
 	for(struct lh_entry *entry ## key = json_object_get_object(obj)->head, *entry_next ## key = NULL; \
 		({ if(entry ## key) { \
 			key = (char*)entry ## key->k; \
