5fb63a09f9bda16d244c298078ce868e7ce587d2
Eswar Yaganti <naga.malleswara.rao.yaganti@intel.com>
2016-06-25 22:50:36 +0530

    linkhash.c: optimised the table_free path

--- linkhash.c
+++ linkhash.c
@@ -562,10 +562,9 @@ int lh_table_resize(struct lh_table *t, int new_size)
 void lh_table_free(struct lh_table *t)
 {
 	struct lh_entry *c;
-	for(c = t->head; c != NULL; c = c->next) {
-		if(t->free_fn) {
+	if(t->free_fn) {
+		for(c = t->head; c != NULL; c = c->next)
 			t->free_fn(c);
-		}
 	}
 	free(t->table);
 	free(t);
