a47eafe868d5a6fe9a0b7bc8552005fdaf8a2075
Eric Haszlakiewicz <erh+git@nimenees.com>
2017-12-05 04:32:12 +0000

    Cast to int64_t instead of long in test_int_add to fix the test on 32-bit platforms.

--- tests/test_int_add.c
+++ tests/test_int_add.c
@@ -13,13 +13,13 @@ int main(int argc, char **argv)
 	tmp = json_object_new_int(INT32_MAX);
 	json_object_int_inc(tmp, 100);
 	assert(json_object_get_int(tmp) == INT32_MAX);
-	assert(json_object_get_int64(tmp) == (long)INT32_MAX + 100L);
+	assert(json_object_get_int64(tmp) == (int64_t)INT32_MAX + 100L);
 	json_object_put(tmp);
 	printf("INT ADD OVERFLOW PASSED\n");
 	tmp = json_object_new_int(INT32_MIN);
 	json_object_int_inc(tmp, -100);
 	assert(json_object_get_int(tmp) == INT32_MIN);
-	assert(json_object_get_int64(tmp) == (long)INT32_MIN - 100L);
+	assert(json_object_get_int64(tmp) == (int64_t)INT32_MIN - 100L);
 	json_object_put(tmp);
 	printf("INT ADD UNDERFLOW PASSED\n");
 	tmp = json_object_new_int64(321321321);
