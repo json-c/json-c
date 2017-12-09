0631c37c7f51231787b0d5611c7712856820e95e
Eric Haszlakiewicz <erh+git@nimenees.com>
2017-12-09 14:59:48 -0500

    Update the master branch to version 0.13.99

--- json_c_version.h
+++ json_c_version.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 Eric Haszlakiewicz
+ * Copyright (c) 2012,2017 Eric Haszlakiewicz
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the MIT license. See COPYING for details.
@@ -13,12 +13,12 @@
 #define _json_c_version_h_
 
 #define JSON_C_MAJOR_VERSION 0
-#define JSON_C_MINOR_VERSION 12
+#define JSON_C_MINOR_VERSION 13
 #define JSON_C_MICRO_VERSION 99
 #define JSON_C_VERSION_NUM ((JSON_C_MAJOR_VERSION << 16) | \
                             (JSON_C_MINOR_VERSION << 8) | \
                             JSON_C_MICRO_VERSION)
-#define JSON_C_VERSION "0.12.99"
+#define JSON_C_VERSION "0.13.99"
 
 /**
  * @see JSON_C_VERSION
