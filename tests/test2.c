dfcac701cc8e083a10c7e302e1eb1c02cba7cbba
hoaianhkhang <46699312+hoaianhkhang@users.noreply.github.com>
2019-12-10 21:46:06 -0500

    Update ifdef TEST_FORMATTED

--- tests/test2.c
+++ tests/test2.c
@@ -8,23 +8,17 @@
 
 #ifdef TEST_FORMATTED
 #define json_object_to_json_string(obj) json_object_to_json_string_ext(obj,sflags)
-#else
-/* no special define */
 #endif
-
 
 int main(int argc, char **argv)
 {
 	json_object *new_obj;
 #ifdef TEST_FORMATTED
 	int sflags = 0;
+	sflags = parse_flags(argc, argv);
 #endif
 
 	MC_SET_DEBUG(1);
-
-#ifdef TEST_FORMATTED
-	sflags = parse_flags(argc, argv);
-#endif
 
 	new_obj = json_tokener_parse("/* more difficult test case */"
 				     "{ \"glossary\": { \"title\": \"example glossary\", \"GlossDiv\": { \"title\": \"S\", \"GlossList\": [ { \"ID\": \"SGML\", \"SortAs\": \"SGML\", \"GlossTerm\": \"Standard Generalized Markup Language\", \"Acronym\": \"SGML\", \"Abbrev\": \"ISO 8879:1986\", \"GlossDef\": \"A meta-markup language, used to create markup languages such as DocBook.\", \"GlossSeeAlso\": [\"GML\", \"XML\", \"markup\"] } ] } } }");
