5f3bf70f039920d31a43a21f36a2ac02df41b7cd
Eric Haszlakiewicz <erh+git@nimenees.com>
2020-06-29 02:18:34 +0000

    Make sure TEST_PARSE_CHUNKSIZE is valid if it's set.

--- tests/test_parse.c
+++ tests/test_parse.c
@@ -42,6 +42,7 @@ static void single_incremental_parse(const char *test_string, int clear_serializ
 	json_object *all_at_once_obj, *new_obj;
 	const char *all_at_once_str, *new_str;
 
+	assert(chunksize > 0);
 	all_at_once_obj = json_tokener_parse(test_string);
 	if (clear_serializer)
 		do_clear_serializer(all_at_once_obj);
