1ee12100b63344d228f2fa0533a551b71c27a85c
Eric Hawicz <erh+git@nimenees.com>
2023-08-05 22:11:30 -0400

    PR #679: add workaround for old compilers w/o stdint.h (i.e. VS2008 and earlier)

--- json_inttypes.h
+++ json_inttypes.h
@@ -13,7 +13,15 @@
 #include <inttypes.h>
 
 #else
+#ifdef JSON_C_HAVE_STDINT_H
 #include <stdint.h>
+#else
+/* Really only valid for old MS compilers, VS2008 and earlier: */
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+#endif
 
 #define PRId64 "I64d"
 #define SCNd64 "I64d"
