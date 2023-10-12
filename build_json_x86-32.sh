f2403a7756057044dfc276df3c993a01a0834dc6
Stephen Dickey <sdickey2001@gmail.com>
2023-10-12 11:35:12 -0700

    build_json*.sh scripts for ARM and x86

---
+++ build_json_x86-32.sh
@@ -0,0 +1,6 @@
+git clean -xfd
+autogen.sh
+./configure --build x86_64-pc-linux-gnu --host arm-linux-gnueabi LDFLAGS="-static -pthread" --enable-mpers=check
+
+# cmake -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_COMPILER=arm-linux-gnueabi-gcc ../json-c
+# make
