51bcdd0000263d6e768911d1ac5f5733dbc344d9
Careles <zhancheng1996@gmail.com>
2025-01-06 10:56:53 +0800

    fix macro WIN32_LEAN_AND_MEAN redefined
    json-c as add_subdirectory and project cmakelists.txt has add_definitions(-DWIN32_LEAN_AND_MEAN)

--- linkhash.c
+++ linkhash.c
@@ -25,7 +25,9 @@
 #endif
 
 #if defined(_MSC_VER) || defined(__MINGW32__)
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
 #include <windows.h> /* Get InterlockedCompareExchange */
 #endif
 
