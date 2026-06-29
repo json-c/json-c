62fa502ec162212d6abad3aaedd2adc14f09d595
Dominik Kummer <6597386+arkadesOrg@users.noreply.github.com>
2026-06-30 01:31:40 +0200

    Disable _user_delete callback after call
    Disabling _user_delete callback after call in _json_object_put_maybe_free() prevents a second call in final assert of json_object_put()

--- json_object.c
+++ json_object.c
@@ -303,7 +303,9 @@ static inline int _json_object_put_maybe_free(struct json_object *jso, int free_
 
 	if (jso->_user_delete)
 		jso->_user_delete(jso, jso->_userdata);
-
+	
+    jso->_user_delete = NULL; // to avoid second call
+	
 	switch (jso->o_type)
 	{
 	case json_type_object: 
